25d4170233d3601ba644831d86fe8deb672b8ea3
kawano-fusic <kawano@fusic.co.jp>
2019-01-20 19:00:16 +0900

    first commit

---
+++ src/components/ChatBoard.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-list three-line>
+    <template v-for="(comment, index) in comments">
+      <v-list-tile
+          :key="index"
+          avatar
+      >
+        <v-list-tile-avatar>
+          <img :src="comment.avatar">
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-sub-title class="text--primary subheading">{{comment.content}}</v-list-tile-sub-title>
+          <v-list-tile-sub-title>
+            {{comment.createdAt.toDate().toLocaleString()}}
+            <v-icon color="red" @click="deleteComment(comment.id)" small>delete</v-icon>
+            <v-btn fab dark small color="pink">
+              <v-icon dark>favorite</v-icon>
+            </v-btn>
+          </v-list-tile-sub-title>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+        </v-list-tile-action>
+      </v-list-tile>
+      <v-divider :key="comment.id"></v-divider>
+    </template>
+  </v-list>
+</template>
+
+<script>
+  import {db} from '../plugins/firebase';
+
+  export default {
+    name: "ChatBoard",
+    data: () => ({
+      comments: [],
+    }),
+    firestore() {
+      return {
+        // firestoreのcommentsコレクションを参照
+        comments: db.collection('comments').orderBy('createdAt', 'desc')
+
+      }
+    },
+    methods: {
+      deleteComment(id) {
+        if (!confirm('コメントを削除してよろしいですか？')) {
+          return
+        }
+        db.collection('comments').doc(id).delete()
+      },
+    },    
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import './plugins/vuetify'
+import './plugins/firebase'
 import App from './App.vue'
 
 Vue.config.productionTip = false
